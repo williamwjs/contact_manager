4393d3b8e41a976f18f35632fbdf6d7c7bc4452d
williamwjs <williamwjs.sjtu@gmail.com>
2015-08-28 19:48:26 -0700

    Test cases finished

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Person, :type => :model do
+  let(:person) do
+    Person.new(first_name: 'Alice', last_name: 'Smith')
+  end
+
+  it 'is valid' do
+    expect(person).to be_valid
+  end
+
+  it 'is invalid without a first name' do
+    person.first_name = nil
+    expect(person).to_not be_valid
+  end
+
+  it 'is invalid without a last name' do
+    person.last_name = nil
+    expect(person).to_not be_valid
+  end
 end
